f2a24a8be12672d20b4f7fc45878f54d04ff5e45
Your Name <youremail@yourdomain.com>
2026-03-12 18:42:19 +0200

    saves

---
+++ src/pages/calc/model/CalcOperations.ts
@@ -0,0 +1,8 @@
+export const CalcOperations = {
+  add: "add",
+  sub: "sub",
+  div: "div",
+  mul: "mul"
+} as const;
+
+export type CalcOperations = typeof CalcOperations[keyof typeof CalcOperations];
